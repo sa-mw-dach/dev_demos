87ca157d702d250e5a9d68f4883d0c8843a1ace6
Manuel Hahn <manuel.hahn@gmx.de>
2022-06-22 12:40:50 +0200

    Finalizing changes for air-gapped env. Plus clean-up.

--- demos/01_license_plate/air-gapped_run_server.sh
+++
@@ -1,3 +0,0 @@
-. /tmp/.venv/bin/activate
-cd dev_demos/external/licence-plate-workshop/ 
-FLASK_ENV=development FLASK_APP=wsgi.py flask run
--- demos/01_license_plate/run_server.sh
+++ demos/01_license_plate/run_server.sh
@@ -1,6 +1,3 @@
-cd /projects/dev_demos/
-python3 -m venv /tmp/.venv && . /tmp/.venv/bin/activate
-python -m pip install --upgrade pip
-pip install -r external/licence-plate-workshop/requirements.txt
-cd external/licence-plate-workshop/ 
+. /tmp/.venv/bin/activate
+cd dev_demos/external/licence-plate-workshop/ 
 FLASK_ENV=development FLASK_APP=wsgi.py flask run
