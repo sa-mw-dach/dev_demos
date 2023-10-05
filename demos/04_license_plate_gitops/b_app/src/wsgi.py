3f587379db781437314acf804df5a075b28c6d68
Manuel Hahn <null>
2023-10-05 17:24:20 +0000

    changed to proper pod name

--- demos/04_license_plate_gitops/b_app/src/wsgi.py
+++ demos/04_license_plate_gitops/b_app/src/wsgi.py
@@ -15,7 +15,7 @@ def status():
 def test_prediction():
 
     # Send test request to model
-    response = requests.post('http://127.0.0.1:5000/predictions', '{"data": "Cars374.png"}')
+    response = requests.post('http://licenseplate:5000/predictions', '{"data": "Cars374.png"}')
     print(response.json())
 
     # Return model response
