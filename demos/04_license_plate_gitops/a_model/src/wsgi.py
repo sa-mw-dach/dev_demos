dbd7ef356dd9bcb298876568b6d16ff2d6a8668f
Manuel Hahn <null>
2023-10-05 17:39:57 +0000

    Added version string to prediction output

--- demos/04_license_plate_gitops/a_model/src/wsgi.py
+++ demos/04_license_plate_gitops/a_model/src/wsgi.py
@@ -4,15 +4,18 @@ from prediction import predict
 
 application = Flask(__name__)
 
+VERSION = 'v4'
 
 @application.route('/')
 @application.route('/status')
 def status():
-    return jsonify({'model status': 'ok (v4)'})
+    return jsonify({'model status': 'ok (' + VERSION + ')'})
 
 
 @application.route('/predictions', methods=['POST'])
 def object_detection():
     data = request.data or '{}'
     body = json.loads(data)
-    return jsonify(predict(body))
+    res = predict(body)
+    res['model version'] = VERSION
+    return jsonify(res)
