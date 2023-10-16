04f48603728b880e22fad54dbe42365320cced4a
Manuel Hahn <manuel.hahn@gmx.de>
2023-10-16 10:01:12 +0200

    Added 4th demo to showcase integrating an AI/ML model into an application.

---
+++ demos/04_license_plate_gitops/b_app/src/config.py
@@ -0,0 +1,9 @@
+import os
+
+workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
+threads = int(os.environ.get('GUNICORN_THREADS', '1'))
+
+forwarded_allow_ips = '*'
+secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+
+bind = "0.0.0.0:8080"
